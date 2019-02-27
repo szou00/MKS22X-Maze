e36574f67adfbc412872b39e6013a4882791baed
szou00 <szou00@stuy.edu>
2019-02-27 10:37:58 -0500

    finally got it to read in the right num of chars

--- ReadFile.java
+++ ReadFile.java
@@ -14,26 +14,28 @@ public class ReadFile {
     }
   }
 
-  public static int readtoArray() throws FileNotFoundException {
+  public static void readtoArray() throws FileNotFoundException {
+    int c = 0;
     int l = 0;
     File text = new File("Maze1.txt");
     Scanner in = new Scanner(text);
     String line;
     while (in.hasNextLine()) {
       line = in.nextLine();
+      l+=1;
       for (int i=0;i<line.length();i++) {
-        if (line.substring(i,i+1) != " ") {
-          l+=1;
-        }
+        // if (line.charAt(i) != ' ') {
+          c+=1;
+        // }
       }
     }
-    return l;
+    System.out.println(l + " " + c);
   }
 
   public static void main(String args[])  {
     try {
       // read();
-      System.out.println(readtoArray());
+      readtoArray();
     }
     catch(FileNotFoundException e) {
       System.out.print("File Not Found");
