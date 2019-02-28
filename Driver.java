613f4e23678054ad479720daa1cbb0c9efa91fa4
szou00 <szou00@stuy.edu>
2019-02-28 10:52:06 -0500

    tested toString and constructor

---
+++ Driver.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Driver {
+
+  public static void main (String args[]) {
+    try {
+      Maze test = new Maze("Maze1.txt");
+      System.out.println(test);
+    }
+    catch (FileNotFoundException e) {
+      System.out.println("no");
+    }
+  }
+}
