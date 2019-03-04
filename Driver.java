e7531c8ec7547c9a869c49da244884633b5bdfb7
Sharon Zou <szou00@stuy.edu>
2019-03-04 06:23:14 -0500

    added mazes from website and tested them

--- Driver.java
+++ Driver.java
@@ -8,9 +8,23 @@ public class Driver {
     try {
       Maze test = new Maze("Maze1.txt");
       System.out.println(test);
-      // System.out.println(test.findEr() + " " + test.findEc());
       System.out.println(test.solve());
       System.out.println(test);
+
+      Maze test2 = new Maze("data1.dat");
+      System.out.println(test2);
+      System.out.println(test2.solve());
+      System.out.println(test2);
+
+      Maze test3 = new Maze("data2.dat");
+      System.out.println(test3);
+      System.out.println(test3.solve());
+      System.out.println(test3);
+
+      Maze test4 = new Maze("data3.dat");
+      System.out.println(test4);
+      System.out.println(test4.solve());
+      System.out.println(test4);
     }
     catch (FileNotFoundException e) {
       System.out.println(e);
